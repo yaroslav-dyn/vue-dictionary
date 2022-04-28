f6d3144d532183b5b31a4f3766df032c414ba16e
Yaroslav Kurilin <yaroslav.kurilin.z@gmail.com>
2022-04-28 12:53:42 +0300

    Change to new API URL

--- src/Services/api.service.ts
+++ src/Services/api.service.ts
@@ -3,13 +3,13 @@ export const apiService = {
 };
 
 const headers = {
-    "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+    "x-rapidapi-host": "mashape-community-urban-dictionary",
     "x-rapidapi-key": "KB5e7ZbqyKmshXFLZRFguVieeyePp1BhDXPjsnvByN3nYiesPM"
 }
 
 
  export function getTranslate (props: string) {
-   return  fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${props}`, {
+     return fetch(`https://api.urbandictionary.com/v0/define?term=${props}`, {
         "method": "GET",
         "headers": headers
     }).then(response => {
